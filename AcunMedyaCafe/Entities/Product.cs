46725fc5651f83a1b424e585ef326c4e2aebd7c2
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-09 20:39:07 +0300

    09.03.2025 - Adım 1

---
+++ AcunMedyaCafe/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace AcunMedyaCafe.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
