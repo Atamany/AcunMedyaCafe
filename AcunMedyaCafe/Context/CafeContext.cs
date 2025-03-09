91a6ec9260038fd9d38c0ea76d7f8a40403c202a
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-09 21:00:51 +0300

    09.03.2025 - Adım 3

---
+++ AcunMedyaCafe/Context/CafeContext.cs
@@ -0,0 +1,17 @@
+﻿using AcunMedyaCafe.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunMedyaCafe.Context
+{
+    public class CafeContext : DbContext
+    {
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=YIGITATAMANPC;initial catalog=DbAcunMedyaCafe;integrated Security=true; TrustServerCertificate=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
