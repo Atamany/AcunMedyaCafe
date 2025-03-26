29c66a51f8132b9258f9c67ad96a495d89905f73
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-26 21:09:15 +0300

    26.03.2025 - Adım 2

---
+++ AcunMedyaCafe/ViewComponents/_AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using AcunMedyaCafe.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaCafe.ViewComponents
+{
+    public class _AdminLayoutNavbarComponentPartial : ViewComponent
+    {
+        private readonly CafeContext _context;
+
+        public _AdminLayoutNavbarComponentPartial(CafeContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+    }
+}
