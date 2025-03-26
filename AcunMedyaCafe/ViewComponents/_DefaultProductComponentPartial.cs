98c5b47c5b763b6f3d5a261e38d2e668b144a233
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-26 17:16:59 +0300

    22.03.2025 - Adım 4

---
+++ AcunMedyaCafe/ViewComponents/_DefaultProductComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using AcunMedyaCafe.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaCafe.ViewComponents
+{
+    public class _DefaultProductComponentPartial : ViewComponent
+    {
+        private readonly CafeContext _context;
+
+        public _DefaultProductComponentPartial(CafeContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var value = _context.Products.ToList();
+            return View(value);
+
+        }
+    }
+}
