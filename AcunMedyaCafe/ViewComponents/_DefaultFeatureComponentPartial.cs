c987920f96a6108f71c0934ea1806b3f768f6e0e
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-03-26 15:57:36 +0300

    22.03.2025 - Adım 1

--- AcunMedyaCafe/ViewComponents/_DefaultFeatureComponentPartial.cs
+++ AcunMedyaCafe/ViewComponents/_DefaultFeatureComponentPartial.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcunMedyaCafe.Context;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AcunMedyaCafe.ViewComponents
 {
     public class _DefaultFeatureComponentPartial : ViewComponent
     {
+        private readonly CafeContext _context;
+
+        public _DefaultFeatureComponentPartial(CafeContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
+            var value = _context.Features.ToList();
             return View();
         }
     }
