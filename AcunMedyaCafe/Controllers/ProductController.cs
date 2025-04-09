fd040a9955ad24e4c9568342c1db4a00b1832093
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-04-09 20:34:20 +0300

    09.04.2025 - Adım 1

---
+++ AcunMedyaCafe/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using AcunMedyaCafe.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcunMedyaCafe.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly CafeContext db;
+        public ProductController(CafeContext db) { 
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var values = db.Products.Include(x=>x.Category).ToList();
+            return View(values);
+        }
+    }
+}
