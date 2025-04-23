fb8cd0ecd2ad4cfce34e48fc3468d4a11b2afbd9
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-04-23 19:02:24 +0300

    Abonelik kısmı dinamik hale getirildi!

--- AcunMedyaCafe/Controllers/DefaultController.cs
+++ AcunMedyaCafe/Controllers/DefaultController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AcunMedyaCafe.Context;
+using AcunMedyaCafe.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AcunMedyaCafe.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly CafeContext _context;
+
+        public DefaultController(CafeContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Subscriber subscriber)
+        {
+            _context.Subscribers.Add(subscriber);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
